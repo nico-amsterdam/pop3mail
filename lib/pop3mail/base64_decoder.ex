e4f052600aa5ea9e03a2b654202419d352d08a0c
nico@amsterdam <n.g.c.hoogervorst@gmail.com>
2016-06-11 01:23:51 +0200

    Document update.

--- lib/pop3mail/base64_decoder.ex
+++ lib/pop3mail/base64_decoder.ex
@@ -1,12 +1,17 @@
 defmodule Pop3mail.Base64Decoder do
 
-   @moduledoc "Replaceable base64 decoder. Replace with your own implementation via the application config :pop3mail, base64_decoder: <replacement>."
+   @moduledoc """
+   Replaceable base64 decoder. Replace with your own implementation via the application config :pop3mail, base64_decoder: &lt;replacement&gt;.
+
+   After changing the config/config.exs run:
+   * mix deps.compile pop3mail
+   """
 
    @base64_decoder Application.get_env(:pop3mail, :base64_decoder) || Pop3mail.Base64Decoder.Standard
 
    defmodule Standard do
 
-     @moduledoc "Standard Erlang base64 decoder"
+     @moduledoc "Standard Elixir base64 decoder"
 
      @doc "Decode base64 encoded text. Returns binary."
      def decode!(encoded_text) do
