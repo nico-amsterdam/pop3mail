4f2a622e18bf2fb04d45444f53d3557babd6c87b
nico@amsterdam <n.g.c.hoogervorst@gmail.com>
2016-06-11 01:06:20 +0200

    fix it

--- lib/pop3mail/base64_decoder.ex
+++ lib/pop3mail/base64_decoder.ex
@@ -2,7 +2,9 @@ defmodule Pop3mail.Base64Decoder do
 
    @moduledoc "Replaceable base64 decoder. Replace with your own implementation via the application config :pop3mail, base64_decoder: <replacement>."
 
-   use Logger
+   @base64_decoder Application.get_env(:pop3mail, :base64_decoder) || Pop3mail.Base64Decoder.Standard
+
+   require Logger
    
    defmodule Standard do
 
@@ -10,17 +12,13 @@ defmodule Pop3mail.Base64Decoder do
 
      @doc "Decode base64 encoded text. Returns binary."
      def decode!(encoded_text) do
+        Logger.debug Application.get_env(:pop3mail, :base64_decoder)
         Base.decode64!(encoded_text, ignore: :whitespace)
      end
 
    end
 
-   def get_decoder do
-      Logger.debug Application.get_env(:pop3mail, :base64_decoder)
-      Application.get_env(:pop3mail, :base64_decoder) || Pop3mail.Base64Decoder.Standard
-   end
-
    @doc "Decode base64 encoded text. Returns binary."
-   defdelegate decode!(encoded_text), to: get_decoder()
+   defdelegate decode!(encoded_text), to: @base64_decoder
 
 end
