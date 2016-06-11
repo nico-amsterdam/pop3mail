db133e5d58d41874efb639da5fd4e46c54dda550
nico@amsterdam <n.g.c.hoogervorst@gmail.com>
2016-06-11 11:34:47 +0200

    new hex version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pop3mail.Mixfile do
 
   def project do
     [app: :pop3mail,
-     version: "0.1.5",
+     version: "0.1.6",
      elixir: "~> 1.2",
      description: description,
      package: package,
