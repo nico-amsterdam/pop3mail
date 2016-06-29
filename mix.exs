7cf00e5933112537ef7f54f8e1901809690605f4
nico@amsterdam <n.g.c.hoogervorst@gmail.com>
2016-06-29 23:45:50 +0200

    documentation layout fixes

--- mix.exs
+++ mix.exs
@@ -30,7 +30,6 @@ defmodule Pop3mail.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [ {:erlpop, github: "trifork/erlpop"},
-      # {:earmark,  "~> 0.1", only: :dev},
       {:ex_doc,  "~> 0.12", only: :dev},
       {:credo,  "~> 0.4"  , only: :dev}
     ]
