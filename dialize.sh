6be90a7067fee6dcde988ffed184f19bd9f70e81
Nico Hoogervorst <16443840+nico-amsterdam@users.noreply.github.com>
2018-08-04 22:08:55 +0200

    Add spec for dializer

--- dialize.sh
+++ dialize.sh
@@ -1,5 +1,4 @@
 mix compile && dialyzer --verbose --no_check_plt -Werror_handling -Wrace_conditions -Wunderspecs -Wunknown -pa /usr/local/lib/elixir/lib/elixir/ebin ../erlpop/ebin -r _build/dev/lib/pop3mail/ebin
 # -Wunmatched_returns
 # dialyzer -pa /usr/local/lib/elixir/lib/elixir/ebin --build_plt --apps erts kernel stdlib httpd inet http_uri crypto ssl os ets -r /usr/local/lib/elixir/lib/elixir/ebin
-#  dialyzer -pa /usr/local/lib/elixir/lib/elixir/ebin  --add_to_plt --apps
-dialyzer -pa /usr/local/lib/elixir/lib/logger/ebin --add_to_plt --apps logger
+# dialyzer -pa /usr/local/lib/elixir/lib/logger/ebin --add_to_plt --apps logger
