ac298acade5c97ecea08b2550af288ea846c87df
Nico Hoogervorst <16443840+nico-amsterdam@users.noreply.github.com>
2018-08-09 21:57:17 +0200

    Upgrade to Elixir 1.6.6

--- mix.exs
+++ mix.exs
@@ -2,23 +2,26 @@ defmodule Pop3mail.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :pop3mail,
-     version: "1.3.0",
-     elixir: "~> 1.3",
+    [
+      app: :pop3mail,
+      version: "1.3.0",
+      elixir: "~> 1.3",
 
-     # Hex
-     package: package(),
-     description: description(),
+      # Hex
+      package: package(),
+      description: description(),
 
-     # Docs
-     docs: [source_ref: "master", main: "Pop3mail",
-            canonical: "http://hexdocs.pm/pop3mail",
-            source_url: "https://github.com/nico-amsterdam/pop3mail"
-           ],
-
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+      # Docs
+      docs: [
+        source_ref: "master",
+        main: "Pop3mail",
+        canonical: "http://hexdocs.pm/pop3mail",
+        source_url: "https://github.com/nico-amsterdam/pop3mail"
+      ],
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -38,9 +41,10 @@ defmodule Pop3mail.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [ {:erlpop, github: "nico-amsterdam/erlpop"},
-      {:ex_doc,  "~> 0.14", only: :dev},
-      {:credo,  "~> 0.5"  , only: :dev}
+    [
+      {:erlpop, github: "nico-amsterdam/erlpop"},
+      {:ex_doc, "~> 0.18.0", only: :dev},
+      {:credo, "~> 0.5", only: :dev}
     ]
   end
 
@@ -55,10 +59,10 @@ defmodule Pop3mail.Mixfile do
 
   defp package do
     [
-     name: :pop3mail,
-     maintainers: ["Nico Hoogervorst"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/nico-amsterdam/pop3mail"}
+      name: :pop3mail,
+      maintainers: ["Nico Hoogervorst"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nico-amsterdam/pop3mail"}
     ]
   end
 end
